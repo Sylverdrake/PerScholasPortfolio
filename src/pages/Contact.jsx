c3f34cee3a6a335063061e0cd7af24663919c969
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-07-31 16:50:21 -0400

    added buttons

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 function Contact() {
+
 return (
-    <div>
-        <div className="conInfo">
-            Contact me through Email
-            or
+    <div className='conWrapper'>
+        <div className="conBox">
+<h1>If you would like to contact me, you can email through: 
+    <br/>cavitevitch@gmail.com</h1>
             <div className="socials">
-            <button>LinkedIn</button>
-            <button>GitHub</button>
-        </div>
+            <button className='button' id='linkedin'onClick={() => window.open('https://www.linkedin.com/in/carl-vitevitch/')}><span className="li">LinkedIn</span></button>
+            <button className='button' id ='github' onClick={() => window.open('https://github.com/Sylverdrake')}><span className='gb'></span>GitHub</button>
+            </div>
         </div>
 
     </div>
