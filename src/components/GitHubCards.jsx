fe3ca40c2873c04876030ef48f769e6c4edc4ae6
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-08-03 15:03:33 -0400

    added api, testing features

--- src/components/GitHubCards.jsx
+++ src/components/GitHubCards.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
 
 function GitHubCards() {
+const [repo, setRepo] = useState([])
+useEffect(() => {getRepo()}, [])
 
+    const getRepo = async () =>
+    {
+        try {
+            const response = await axios.get('https://api.github.com/users/Sylverdrake/repos')
+            const myRepos = response.data;
+            setRepo(myRepos);
+        }
+        catch (err)
+        {
+            console.error('Cannot get repos', err)
+        }
+    }
+    console.log(repo[0])
 return (
-    <div>GitHubCards</div>
+    <div>
+        <div>
+            Test: {repo[22]?.full_name}
+        </div>
+
+    </div>
 )
 }
-
 export default GitHubCards
+
