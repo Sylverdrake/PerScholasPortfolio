928c23a41e2c19caefb228861696e647b48955dc
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-08-03 15:44:12 -0400

    adjusting design

--- src/components/GitHubCards.jsx
+++ src/components/GitHubCards.jsx
@@ -23,23 +23,23 @@ return (
 <div>
     <div className='portfolio'>
             <div className='featured'>
-            <p>Test: {repo[19]?.full_name}</p>
+            <p>{repo[19]?.name}</p>
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
                 
                 </div>        
             <div className='featured'>
-                Test: {repo[22]?.full_name}
+                <p>{repo[22]?.name}</p>
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
             </div>
     </div>
 
     <div className='portfolio'>
             <div className='featured'>
-                Test: {repo[25]?.full_name}
+                <p>{repo[25]?.name}</p>
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
             </div>        
             <div className='featured'>
-                Test: {repo[28]?.full_name}
+                <p>{repo[28]?.name}</p>
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
             </div>
     </div>
