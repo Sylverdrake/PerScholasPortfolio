261264dfd0a9372023002a481e41bb9629f15f74
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-08-03 16:04:56 -0400

    reworking design

--- src/components/GitHubCards.jsx
+++ src/components/GitHubCards.jsx
@@ -23,32 +23,42 @@ return (
 <div>
     <div className='portfolio'>
             <div className='featured'>
-            <p>{repo[19]?.name}</p>
-            <p>{repo[19]?.description}</p>
-            <p>Written in: {repo[19]?.language}</p>
+                <span>{repo[19]?.name}</span>
+
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
-                
-                </div>        
+                <div className='fInfo'>
+                    <p>{repo[19]?.description}</p>
+                    <p>Written in: {repo[19]?.language}</p>
+                </div>
+            </div>        
+            
             <div className='featured'>
-                <p>{repo[22]?.name}</p>
-                <p>{repo[22]?.description}</p>
-                <p>Written in: {repo[22]?.language}</p>
+                <span>{repo[22]?.name}</span>
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
+                <div className='fInfo'>
+                    <p>{repo[22]?.description}</p>
+                    <p>Written in: {repo[22]?.language}</p>
+                </div>
             </div>
     </div>
 
     <div className='portfolio'>
             <div className='featured'>
-                <p>{repo[25]?.name}</p>
-                <p>{repo[25]?.description}</p>
-                <p>Written in: {repo[25]?.language}</p>
+                <span>{repo[25]?.name}</span>
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
-            </div>        
+                <div className='fInfo'>
+                    <p>{repo[25]?.description}</p>
+                    <p>Written in: {repo[25]?.language}</p>
+                </div>
+            </div>  
+
             <div className='featured'>
-                <p>{repo[28]?.name}</p>
-                <p>{repo[28]?.description}</p>
-                <p>Written in: {repo[28]?.language}</p>
+                <span>{repo[28]?.name}</span>
                 <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt=''/>
+                <div className='fInfo'>
+                    <p>{repo[28]?.description}</p>
+                    <p>Written in: {repo[28]?.language}</p>
+                </div>
             </div>
     </div>
 </div>
