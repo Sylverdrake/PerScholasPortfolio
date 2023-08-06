a4a54c51d0d5a0ace2391b2234b46472280168a2
Carl V <126032225+Sylverdrake@users.noreply.github.com>
2023-08-06 18:18:26 -0400

    removed filter component, will work on it later

--- src/components/GitHubCards.jsx
+++ src/components/GitHubCards.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
 
-import Filter from './Filter';
 
 function GitHubCards() {
-const [repo, setRepo] = useState([])
+const [repo, setRepo] = useState([]);
+
 useEffect(() => {getRepo()}, [])
 
     const getRepo = async () =>
@@ -22,7 +22,6 @@ useEffect(() => {getRepo()}, [])
     }
 return (
 <div>
-    <Filter/>
     <div className='portfolio'>
             <div className='featured'>
                     <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt='' onClick={() => window.open(`${repo[19]?.html_url}`)}/>
@@ -49,7 +48,7 @@ return (
 
     <div className='portfolio'>
             <div className='featured'>
-                        <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt='' onClick={() => window.open(`${repo[25]?.html_url}`)}/>
+                <img className='fImg' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngkey.com%2Fpng%2Fdetail%2F233-2332677_image-500580-placeholder-transparent.png&f=1&nofb=1&ipt=bf2da49be302c2eb1c0e157402b7672822220d71558f50d70dcaeb867cd71ad0&ipo=images' alt='' onClick={() => window.open(`${repo[25]?.html_url}`)}/>
                 <div className='fInfo'>
                     <h3>Webpage Space Battle</h3>
                     <hr/>
